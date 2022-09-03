9cf8161738fd3fcfd20b31d524d384d6bf06cd31
CrashMaster8497 <crashmaster8497@gmail.com>
2022-09-03 19:02:01 +0800

    feat: note

---
+++ src/CustomerDataLayer/BusinessEntities/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerDataLayer.BusinessEntities
+{
+    public class Note
+    {
+        public int NoteId { get; set; } = 0;
+        public int CustomerId { get; set; } = 0;
+        public string Text { get; set; } = string.Empty;
+    }
+}
---
+++ tests/CustomerDataLayer.Tests/BusinessEntities/NoteTest.cs
@@ -0,0 +1,63 @@
+﻿using CustomerDataLayer.BusinessEntities;
+using FluentAssertions;
+
+namespace CustomerDataLayer.Tests.BusinessEntities
+{
+    public class NoteTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateNote()
+        {
+            var note = new Note();
+
+            note.Should().NotBeNull();
+
+            note.NoteId.Should().BeOfType(typeof(int));
+            note.CustomerId.Should().BeOfType(typeof(int));
+            note.Text.Should().BeOfType<string>();
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultValues()
+        {
+            var note = new Note();
+
+            note.NoteId.Should().Be(0);
+            note.CustomerId.Should().Be(0);
+            note.Text.Should().Be(string.Empty);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetSetNoteId()
+        {
+            var note = new Note
+            {
+                NoteId = 1
+            };
+
+            note.NoteId.Should().Be(1);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetSetCustomerId()
+        {
+            var note = new Note
+            {
+                CustomerId = 1
+            };
+
+            note.CustomerId.Should().Be(1);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetSetText()
+        {
+            var note = new Note
+            {
+                Text = "Text"
+            };
+
+            note.Text.Should().Be("Text");
+        }
+    }
+}
