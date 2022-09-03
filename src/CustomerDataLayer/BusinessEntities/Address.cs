285044cdd95ad8de2110b45f81cbb95b6a145c8a
CrashMaster8497 <crashmaster8497@gmail.com>
2022-09-03 18:54:20 +0800

    feat: address

---
+++ src/CustomerDataLayer/BusinessEntities/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomerDataLayer.BusinessEntities
+{
+    public class Address
+    {
+        public int AddressId { get; set; } = 0;
+        public int CustomerId { get; set; } = 0;
+        public string AddressLine { get; set; } = string.Empty;
+        public string AddressLine2 { get; set; } = string.Empty;
+        public AddressType AddressType { get; set; } = AddressType.Shipping;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+
+    public enum AddressType
+    {
+        Shipping,
+        Billing
+    }
+}
